645fe571f211c19ce971c56dd28f7f7bd397d2ae
bilgeyillmaz <96742976+bilgeyillmaz@users.noreply.github.com>
2022-04-12 17:57:56 +0300

    Add project files.

---
+++ RentACar/Data/RentACarDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentACar.Models;
+
+namespace RentACar.Data
+{
+    public class RentACarDBContext: DbContext
+    {
+        public RentACarDBContext(DbContextOptions<RentACarDBContext> options) : base(options){ }
+        public DbSet<Deneme> Denemeler { get; set; }   
+        
+    }
+}
