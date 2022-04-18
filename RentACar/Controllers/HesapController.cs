46a7031af7fed4782d28e8c0a69bc00bd19475f4
bilgeyillmaz <96742976+bilgeyillmaz@users.noreply.github.com>
2022-04-18 15:30:25 +0300

    .

---
+++ RentACar/Controllers/HesapController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentACar.Controllers
+{
+    public class HesapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Giris()
+        {
+            return View();
+        }
+        public IActionResult Kayit()
+        {
+            return View();
+        }
+        public IActionResult SifreSifirla()
+        {
+            return View();
+        }
+        public IActionResult HukumveKosullar()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Kayit(string UserNamee)
+        {
+            string a = "Ali" + UserNamee;
+            return View(a);
+        }
+    }
+}
---
+++ RentACar/Models/FuelType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACar.Models
+{
+    public class FuelType
+    {
+        [Key]
+        public byte FuelTypeID { get; set; }
+        [Required(ErrorMessage = "Yakıt Tipi boş geçilemez!"), Display(Name = "Yakıt Türü"), StringLength(20, MinimumLength = 3, ErrorMessage = "{0} {2}-{1} Karakter olmalı!")]
+        public string FuelTypeName { get; set; }
+    }
+}
